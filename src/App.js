27afc9aaf344dcaedfb31852c3e9f042c1fdda69
Sergio Varela <sergio.varela@hilasal.superate.org.sv>
2024-10-22 11:20:21 -0600

    Se agregaron las rutas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import { Cajero } from "./State/Cajero"
-import { Nombre2 } from "./State/Nombre2"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Navbar } from "./Navbar"
+import { Login } from "./Login/login";
+import { Registro } from "./Registro/Registro";
 
 export default function App() {
   return (
     <div>
-      <Cajero />
-      <Nombre2 />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/registro' element={<Registro />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
